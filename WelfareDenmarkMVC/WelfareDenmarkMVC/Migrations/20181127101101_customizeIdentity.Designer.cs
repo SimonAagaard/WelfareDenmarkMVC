2a84f592471fd44161bc03b5599955c9494cc034
Christoffer Sohn Ankersen <chri5122@edu.eal.dk>
2018-11-27 14:16:27 +0100

    APPLICATIONUSER ID NOW PASSES TO MY CONTROLLER- JAAAAAAA

--- WelfareDenmarkMVC/WelfareDenmarkMVC/Migrations/20181127101101_customizeIdentity.Designer.cs
+++ WelfareDenmarkMVC/WelfareDenmarkMVC/Migrations/20181127101101_customizeIdentity.Designer.cs
@@ -11,9 +11,10 @@ using WelfareDenmarkMVC.Data;
 namespace WelfareDenmarkMVC.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181127101101_customizeIdentity")]
+    partial class customizeIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
