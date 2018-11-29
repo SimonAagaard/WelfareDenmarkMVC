bb66ab66582b81283e43c0fc20b47eeaa35567da
Christoffer Sohn Ankersen <chri5122@edu.eal.dk>
2018-11-29 10:18:50 +0100

    Temp API

---
+++ WelfareDenmarkMVC/WelfareDenmarkAPI/Models/Checklist/ChecklistContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WelfareDenmarkAPI.Models;
+
+namespace WelfareDenmarkAPI.Models
+{
+    public class ChecklistContext : DbContext
+    {
+        public ChecklistContext(DbContextOptions<ChecklistContext> options) : base(options)
+        {
+
+        }
+        
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            
+        }
+
+        public DbSet<Checklist> Checklists { get; set; }
+    }
+}
